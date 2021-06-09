9301c1e5a6e5b5b3d06444debd0e57cbb3b65075
Amy Dang <kirontoo@gmail.com>
2021-06-09 14:01:21 -0700

    add nav link toggle

--- Challenge01/src/script.js
+++ Challenge01/src/script.js
@@ -1,1 +1,13 @@
 const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener( 'click', () => {
+	// if( navLinks.style.display != 'none' ) {
+	// 	navLinks.style.display = 'none';
+	// } else {
+	// 	navLinks.style.display = 'block';
+	// }
+
+	console.log(navLinks.style.display)
+	navLinks.style.display = ( navLinks.style.display == 'none' ) ? 'block' : 'none';
+});
